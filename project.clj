2bbf64fe96b31b53a3043ba48a8057ddf35c926c
Lei Wang <tendant@gmail.com>
2021-03-02 11:36:45 -0800

    release 0.1.8

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject tendant/common-service "0.1.7"
+(defproject tendant/common-service "0.1.8"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
