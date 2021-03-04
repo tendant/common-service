6c1448acfefd22c2a04004dc02516341d4a587cd
Lei Wang <tendant@gmail.com>
2021-03-04 11:59:27 -0800

    Add first test case

--- test/common/crux_svc_test.clj
+++ test/common/crux_svc_test.clj
@@ -1,7 +1,16 @@
 (ns common.crux-svc-test
   (:require [clojure.test :refer :all]
-            [common.crux-svc :refer :all]))
+            [common.crux-svc :refer :all]
+            [clojure.test.check :as tc]
+            [clojure.test.check.generators :as gen]
+            [clojure.test.check.properties :as prop]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def node (crux.api/start-node {}))
+
+(def prop-create-entity-sync
+  (prop/for-all [params (gen/not-empty (gen/map gen/keyword gen/string-alphanumeric))]
+    (is (create-entity-sync node :entity/my-test params))))
+
+(deftest test-create-entity-sync
+  (testing "Create entity sync"
+    (tc/quick-check 100 prop-create-entity-sync)))
