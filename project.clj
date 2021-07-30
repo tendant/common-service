003fd90776cb1f3082b0c0837970bdcd1f908c50
Yifan Zhou <yifanz3@vt.edu>
2021-07-30 10:04:10 +0800

    update crux

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [juxt/crux-core "20.12-1.13.0-beta"]
-                 [juxt/crux-jdbc "20.12-1.13.0-beta"]
+                 [juxt/crux-core "21.05-1.17.0-beta"]
+                 [juxt/crux-jdbc "21.05-1.17.0-beta"]
                  [yogthos/config "1.1.5"]]
   :repl-options {:init-ns common.crux-svc})
