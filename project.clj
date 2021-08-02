5357dacdc22e4a7f1590bf2bf109dc9e719e4666
Yifan Zhou <yifanz3@vt.edu>
2021-08-02 17:06:51 +0800

    fix dependency

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/test.check "1.1.0"]
                  [juxt/crux-core "21.05-1.17.0-beta"]
                  [juxt/crux-jdbc "21.05-1.17.0-beta"]
                  [yogthos/config "1.1.5"]]
